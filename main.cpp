050026e7db0af7c37839c2ae184399dbd1a66e7e
Shaylin <antelopes13@gmail.com>
2018-05-15 13:01:53 +0200

    more function added to main and test cases added to makefile for tutor convinience

--- main.cpp
+++ main.cpp
@@ -12,18 +12,58 @@ int main(int argc, char* argv[])
 	cout << "Sound processing initiated"<<endl;
 	cout<<"\n";
 	
-	int pos =1;
+	//knowing that the format of the first 5 values must be the same we can assign them as below:
+	
+	int sampleRate = stoi(argv[2]);
+	int sampleSize = stoi(argv[4]);
+	int numberOfChannels = stoi(argv[6]);
+	string outputFileName = "out";
+	
 	string inputFileName;
-	string outputFileName;
-	int sampleSize;
-	int sampleRate;
-	int numberOfChannels;
+	string inputFileName2 = argv[argc-1];
+	
+	string option;
+	
+	string temp = argv[7];
+	if(temp.compare("-o")==0)
+	{
+	 outputFileName = argv[8];
+	 option = argv[9];
+	 inputFileName = argv[10];
+	 
+	}
+	else
+	{
+	 option = argv[7];
+	 inputFileName = argv[8];
+	}
+	
+	//deciding based on option
+	
+	
+	
+	
+	//testing------------------------------------------------
+	cout<<"input file name: "<<inputFileName<<endl;
+	cout<<"input file name2: "<<inputFileName2<<endl;
+	cout<<"sample rate: "<<sampleRate<<endl;
+	cout<<"sample size: "<<sampleSize<<endl;
+	cout<<"number of channels: "<<numberOfChannels<<endl;
+	cout<<"output file name: "<<outputFileName<<endl;
+	cout<<"option: "<<option<<endl;
+	
+	/*cout<<endl;
+	for(int i =1;i<argc ; i++)
+	{
+	    cout<<i<<" "<<argv[i]<<endl;  
+	}*/
+	//--------------------------------------------------------
 	
     
     string infile1 = "beez18sec_44100_signed_8bit_mono.raw";
-    int sampleRate = 44100;
+    int sampleRate2 = 44100;
     //testing
-    Sound<int8_t,1> sample1 (infile1, sampleRate);//Create audio Sample #1,8bit
+    Sound<int8_t,1> sample1 (infile1, sampleRate2);//Create audio Sample #1,8bit
     //sample1.reverse();
     sample1.save("testsample");
     
---
+++ makefile
@@ -0,0 +1,24 @@
+CC=g++
+CCFLAGS=-std=c++11
+LIBS=-lm
+
+driver: main.o
+		$(CC) $(CCFLAGS) main.o -o driver $(LIBS)
+
+				
+driver.o: main.cpp
+		$(CC) $(CCFLAGS) main.cpp -c
+
+
+		
+clean: 
+		@rm -f *.o
+		@rm -f driver
+		
+test1: 
+		./driver -r 44100 -b 8 -c 1 -o output -add file1 file2
+		
+test2: 
+		./driver -r 44100 -b 8 -c 1 -add file1 file2
+		
+		
